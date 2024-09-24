4173cb5555e1286666ceadf9b7bfcb98cd62747f
anastasia.peretyatko <aperetyatko989@gmail.com>
2024-09-24 19:32:39 +0300

    User endpoint

---
+++ src/models/user.model.ts
@@ -0,0 +1,46 @@
+import { Column, DataType, HasOne, Model, Table } from 'sequelize-typescript';
+import { Professor } from './professor.model';
+import * as bcrypt from 'bcrypt';
+import { Student } from './student.model';
+
+@Table({ tableName: 'users' })
+export class User extends Model<User> {
+  @Column({
+    type: DataType.UUID,
+    unique: true,
+    defaultValue: DataType.UUIDV4,
+    primaryKey: true,
+  })
+  id: string;
+
+  @Column({ type: DataType.STRING, unique: true, allowNull: false })
+  email: string;
+
+  @Column({ type: DataType.STRING, allowNull: false })
+  password: string;
+
+  @Column({ type: DataType.STRING, allowNull: false })
+  role: string;
+
+  @Column({ type: DataType.BOOLEAN, defaultValue: false })
+  isActivated: boolean;
+
+  @Column({ type: DataType.STRING })
+  activationLink: string;
+
+  @HasOne(() => Professor, {
+    foreignKey: 'user_id',
+    as: 'professor',
+  })
+  professor: Professor;
+
+  @HasOne(() => Student, {
+    foreignKey: 'user_id',
+    as: 'student',
+  })
+  student: Student;
+
+  static async hashPassword(password: string): Promise<string> {
+    return await bcrypt.hash(password, 10);
+  }
+}
