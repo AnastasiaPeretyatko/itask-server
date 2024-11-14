c3a14e8b86e83a20da06ac31f0ef0f449cb0cbaf
anastasia.peretyatko <aperetyatko989@gmail.com>
2024-11-14 08:25:42 +0300

    TSK-25: create student endpoint

--- src/modules/groups/groups.module.ts
+++ src/modules/groups/groups.module.ts
@@ -3,12 +3,13 @@ import { GroupsController } from './groups.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Group } from 'src/models/group.model';
 import { GroupsService } from './groups.service';
+import { University } from 'src/models/university.model';
 
 @Module({
   controllers: [GroupsController],
   providers: [GroupsService],
   imports:[
-    SequelizeModule.forFeature([Group])
+    SequelizeModule.forFeature([Group, University])
   ]
 })
 export class GroupModule {}
---
+++ src/modules/students/dto/update-student.dto.ts
@@ -0,0 +1,5 @@
+export class CreateStudentDto {
+  readonly groupId: string;
+  readonly fullName: string;
+  readonly tel: string;
+}
