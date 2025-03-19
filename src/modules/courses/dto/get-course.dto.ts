f577036453cf93c0505c70f571e3ebbc9164c926
anastasia.peretyatko <aperetyatko989@gmail.com>
2025-03-19 17:27:39 +0300

    исправление валидации пагинации при получении курса

--- src/modules/courses/dto/get-course.dto.ts
+++ src/modules/courses/dto/get-course.dto.ts
@@ -2,8 +2,8 @@ import { z } from 'zod';
 
 export const GetCoursesSchema = z.object({
   search: z.string().optional().default(''),
-  limit: z.number().optional(),
-  page: z.number().optional(),
+  limit: z.coerce.number().int().positive().optional(),
+  page: z.coerce.number().int().positive().optional(),
 });
 
 export type GetCoursesDto = z.infer<typeof GetCoursesSchema>;
